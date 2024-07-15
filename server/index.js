42dcb5d5798edb03aa0192add2918b11a3fd605e
4hmedmhamed <ah5733956@gmail.com>
2024-07-16 00:18:08 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express =require("express");
+const app =express();
+
+app.use("/", (req, res)=>{
+    res.send("server is connting ")
+});
+
+app.listen(5000 , console.log("sever port 5000"));
